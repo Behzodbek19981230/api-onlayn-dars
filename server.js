a7d2dd1bd4ff986e2c654691f307e30da211c138
unknown <behzodrasulov432@gmail.com>
2024-03-12 10:48:07 +0500

    fixeed port to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const db = require("./config/db");
 const server = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8880;
 
 server.use(cors());
 server.use(express.json());
