a896937db6571e92b9a385765f2f7425f5c635cc
unknown <behzodrasulov432@gmail.com>
2024-03-12 13:01:08 +0500

    firs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const db = require("./config/db");
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 server.use(cors());
 server.use(express.json());
