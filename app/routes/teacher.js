14505f6ed623250383ac0bd4d10b6352fcfc6d22
unknown <behzodrasulov432@gmail.com>
2024-03-19 17:52:05 +0500

    add teacher

--- app/routes/teacher.js
+++ app/routes/teacher.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 const TeacherController = require("../controllers/Teacher");
 const upload = multer({ dest: "uploads/" }); // Specify the directory where uploaded files will be stored
 
-route.get("/", TeacherController.get);
+route.get("/", TeacherController.getAll);
 route.post("/", auth, upload.single("image"), TeacherController.add);
 route.delete("/:id", auth, upload.single("file"), TeacherController.delete);
 route.put("/:id", auth, upload.single("file"), TeacherController.edit);
