404dbf41449b25761cc635c6c1276d1a362737a5
unknown <behzodrasulov432@gmail.com>
2024-03-19 17:50:42 +0500

    add teacher

--- app/routes/teacher.js
+++ app/routes/teacher.js
@@ -1,12 +1,12 @@
 const route = require("express").Router();
 const multer = require("multer");
 const auth = require("../middleware/auth");
-const Teacher = require("../models/Teacher");
+const TeacherController = require("../controllers/Teacher");
 const upload = multer({ dest: "uploads/" }); // Specify the directory where uploaded files will be stored
 
-route.get("/", Teacher.getAll);
-route.post("/", auth, upload.single("image"), Teacher.add);
-route.delete("/:id", auth, upload.single("file"), Teacher.delete);
-route.put("/:id", auth, upload.single("file"), Teacher.edit);
+route.get("/", TeacherController.get);
+route.post("/", auth, upload.single("image"), TeacherController.add);
+route.delete("/:id", auth, upload.single("file"), TeacherController.delete);
+route.put("/:id", auth, upload.single("file"), TeacherController.edit);
 
 module.exports = route;
